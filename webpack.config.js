c561d5abf705cf6273d064e24fcbd4a4643dc9c1
weed <weed@yandex-team.ru>
2019-08-06 13:10:36 +0300

    LX: updated environment and bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-	entry: './src/index.js',
-	module: {
-		rules: [
-			{
-				test: /\.(js)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
-			{
-				test: /\.css$/,
-				use: [
-				  'style-loader',
-				  {
-				    loader: 'css-loader',
-				    options: {
-				      modules: true,
-				    }
-				  },
-				  'stylus-loader'
-				]
-			}
-		],
-	},
-	resolve: {
-		extensions: ['*', '.js', '.css'],
-	},
-	output: {
-		path: __dirname + '/dist',
-		publicPath: '/',
-		filename: 'bundle.js',
-	},
-	devServer: {
-		contentBase: './dist',
-	},
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      modules: true,
+                    }
+                  },
+                  'stylus-loader'
+                ]
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.css'],
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: './dist',
+    },
 };
